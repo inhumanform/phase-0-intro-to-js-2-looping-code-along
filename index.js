32fc7c773ddc0a37d661dab2e9757afc3633bc36
Drew <dch528@gmail.com>
2024-01-17 15:09:16 -0500

    Drew Hairston Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const my_names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, holiday = "birthday"){
+    return names.map(name => `Thank you, ${name}, for the wonderful ${holiday} gift!`);
+}
+
+writeCards(my_names, "surprise");
+
+
+
+function countDown(number) {
+    
+    if (number < 0) {
+      console.log("Please enter a positive integer.");
+      return;
+    }
+  
+    
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+ 
+  countDown(10);
